b90d51b5dfebbc88c98eaca2ff0b25a02c957291
Ermias Wakgari Yadeta <118937954+ERMIASWAKGARI@users.noreply.github.com>
2024-05-11 09:45:17 +0300

    first commit: project setup

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App: React.FC = () => {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <h1 className="text-3xl text-gray-900">Hello, World!</h1>
+    </div>
+  );
+};
+
+export default App;
